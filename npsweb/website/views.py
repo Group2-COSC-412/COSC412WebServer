6420c4d2d9e667ab1f17ccd1a89ef393d75e4ba0
cgood404 <chrisgood404@gmail.com>
2018-04-03 20:06:34 -0400

    change index.html to template

--- npsweb/website/views.py
+++ npsweb/website/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import os
 
 # Create your views here.
+WK_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def index(request):
     template = loader.get_template('website/index.html')
