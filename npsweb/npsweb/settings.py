96cdc06405966d779d14af1fa3a0ff30ab313855
cgood404 <chrisgood404@gmail.com>
2018-04-25 11:38:43 -0400

    Add national-parks.fcgit.net to AllowedHosts

--- npsweb/npsweb/settings.py
+++ npsweb/npsweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(d(du34v67q6gq5wulu4s(j!ad%&5=q2jpjdp9u*z(-p*j&wb9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['national-parks.fcgit.net']
 
 
 # Application definition
