913336275435f80d3dc77b2b24616f0b9b79a79a
cgood404 <chrisgood404@gmail.com>
2018-05-01 12:15:35 -0400

    Add login method

--- npsweb/auth/views.py
+++ npsweb/auth/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, H
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 from django.http import HttpRequest
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 
 def login(request: HttpRequest):
     # if username and password in db, login, otherwise return error
-    pass
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if not user:
+        login(request, user)
+    else:
+        # Return error message
+        pass
