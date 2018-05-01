72829d248c0284019b8451f4433d10845a79a5d2
cgood404 <chrisgood404@gmail.com>
2018-05-01 15:01:13 -0400

    Fix API

--- npsweb/api/views.py
+++ npsweb/api/views.py
@@ -2,46 +2,46 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseForbidden
 
 # Create your views here.
 
 
-def esGET(request: HttpRequest):
-    keys = open("/home/ubuntu/keys/Django-User-AWS.key", 'r')
-    aws_key = keys.readline().replace('\n', '')
-    aws_secret = keys.readline().replace('\n', '')
-    endpoint = keys.readline().replace('\n', '')
-    keys.close()
+def es(request: HttpRequest):
+    if request.method == "GET":
+        keys = open("/home/ubuntu/keys/Django-User-AWS.key", 'r')
+        aws_key = keys.readline().replace('\n', '')
+        aws_secret = keys.readline().replace('\n', '')
+        endpoint = keys.readline().replace('\n', '')
+        keys.close()
 
-    region = "us-east-1"
-    service = "es"
-    aws_auth = AWS4Auth(aws_key, aws_secret, region, service)
-    esnode = Elasticsearch(
-        hosts=[{'host': endpoint, 'port': 443}],
-        http_auth=aws_auth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
-    return HttpResponse('{"comment": "example comment on a park that we pulled from the database"}')
+        region = "us-east-1"
+        service = "es"
+        aws_auth = AWS4Auth(aws_key, aws_secret, region, service)
+        esnode = Elasticsearch(
+            hosts=[{'host': endpoint, 'port': 443}],
+            http_auth=aws_auth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+        return HttpResponse('{"comment": "example comment on a park that we pulled from the database"}')
 
+    else:
+        keys = open("/home/ubuntu/keys/Django-User-AWS.key", 'r')
+        aws_key = keys.readline().replace('\n', '')
+        aws_secret = keys.readline().replace('\n', '')
+        endpoint = keys.readline().replace('\n', '')
+        keys.close()
 
-def esPOST(request: HttpRequest):
-    keys = open("/home/ubuntu/keys/Django-User-AWS.key", 'r')
-    aws_key = keys.readline().replace('\n', '')
-    aws_secret = keys.readline().replace('\n', '')
-    endpoint = keys.readline().replace('\n', '')
-    keys.close()
-
-    region = "us-east-1"
-    service = "es"
-    aws_auth = AWS4Auth(aws_key, aws_secret, region, service)
-    esnode = Elasticsearch(
-        hosts=[{'host': endpoint, 'port': 443}],
-        http_auth=aws_auth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
-    return HttpResponse('')
+        region = "us-east-1"
+        service = "es"
+        aws_auth = AWS4Auth(aws_key, aws_secret, region, service)
+        esnode = Elasticsearch(
+            hosts=[{'host': endpoint, 'port': 443}],
+            http_auth=aws_auth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+        return HttpResponse('')
--- npsweb/auth/views.py
+++ npsweb/auth/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse, H
-from elasticsearch import Elasticsearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
+from django.http import HttpResponse
 from django.http import HttpRequest
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
-def login(request: HttpRequest):
-    # if username and password in db, login, otherwise return error
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if not user:
-        login(request, user)
-    else:
-        # Return error message
+def usrLogin(request: HttpRequest):
+    if request.method == 'GET':
+        # TODO redirect to login page
         pass
+    else:
+        # if username and password in db, login, otherwise return error
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            # TODO redirect to home or profile or something
+        else:
+            # TODO Return error message
+            pass
+
+
+def createUser(request: HttpRequest):
+    # TODO email validation
+    pass
