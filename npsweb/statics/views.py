a03e913ff5dbc8ae4e51ea40ec776f2ad392bdc4
cgood404 <chrisgood404@gmail.com>
2018-05-08 20:52:57 -0400

    maybe works now

--- npsweb/statics/views.py
+++ npsweb/statics/views.py
@@ -6,7 +6,7 @@ import os
 WK_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def css(request):
+def css(request, file):
     pathlist = request.path.split('/')
     file = pathlist[len(pathlist) - 1]
     return HttpResponse(open(WK_DIR+'/css/'+file))
