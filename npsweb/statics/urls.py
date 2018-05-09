cc5705a85bdd1e02e449b143a544e2744c6862e6
cgood404 <chrisgood404@gmail.com>
2018-05-08 20:57:29 -0400

    static files work now

--- npsweb/statics/urls.py
+++ npsweb/statics/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('css/<path:file>', views.css),
-    path('fonts/<path:file>', views.fonts),
-    path('js/<path:file>', views.js)
+    path('statics/<path:file>', views.statics),
 ]
