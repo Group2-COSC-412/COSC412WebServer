047a406a142e8c51cb9385e9eb64751782b4cb3c
cgood404 <chrisgood404@gmail.com>
2018-05-08 21:10:21 -0400

    ..

--- npsweb/website/urls.py
+++ npsweb/website/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.empty, name='redirect'),
+    # path('', views.empty, name='redirect'),
     path('<path:file>', views.index, name='index')
 ]
