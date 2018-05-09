7d7af34fbfd770ad7ce4d74728eca7ba8ea9c558
cgood404 <chrisgood404@gmail.com>
2018-05-08 20:40:41 -0400

    add static files

--- npsweb/statics/views.py
+++ npsweb/statics/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 import os
 
 # Create your views here.
 WK_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def indexstyle(request):
-    return HttpResponse(open(WK_DIR+'/css/indexstyle.css'))
+
+def css(request):
+    pathlist = request.path.split('/')
+    file = pathlist[len(pathlist) - 1]
+    return HttpResponse(open(WK_DIR+'/css/'+file))
+
+
+def fonts(request: HttpRequest):
+    pathlist = request.path.split('/')
+    file = pathlist[len(pathlist) - 1]
+    return HttpResponse(open(WK_DIR+'/css/fonts/'+file))
+
+
+def js(request: HttpRequest):
+    pathlist = request.path.split('/')
+    file = pathlist[len(pathlist) - 1]
+    return HttpResponse(open(WK_DIR+'/js/'+file))
---
+++ npsweb_tests.py
@@ -0,0 +1,13 @@
+from npsweb.api import views as apiview
+from npsweb.auth import views as auth
+from npsweb.statics import views as statics
+from npsweb.website import views as website
+
+
+# TODO
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
