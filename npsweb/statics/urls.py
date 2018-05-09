d0aca90f2c42f07386bca82f145fe032791a3812
cgood404 <chrisgood404@gmail.com>
2018-05-08 20:45:03 -0400

    ..

--- npsweb/statics/urls.py
+++ npsweb/statics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'css/*', views.css),
-    path(r'fonts/*', views.fonts),
-    path(r'js/*', views.js)
+    path(r'css/(.*?)', views.css),
+    path(r'fonts/(.*?)', views.fonts),
+    path(r'js/(.*?)', views.js)
 ]
