ca0402d4928553bb19e752258fb4fe0a459770bb
cgood404 <chrisgood404@gmail.com>
2018-05-12 18:31:36 -0400

    turn debugging off, someone trying to poke for vulnerabilities

--- npsweb/auth/urls.py
+++ npsweb/auth/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login', auth_views.LoginView.as_view(template_name='login.html')),
+    path('login', auth_views.LoginView.as_view(template_name='login.html', next='https://national-parks.fcgit.net/home/states')),
     path('logout', auth_views.LogoutView.as_view(next_page='https://national-parks.fcgit.net/home/index')),
     path('create', views.createUser)
 ]
