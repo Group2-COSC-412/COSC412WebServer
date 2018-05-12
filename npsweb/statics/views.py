ee19d9eebcbca1972988f89259be7b226d84555a
cgood404 <chrisgood404@gmail.com>
2018-05-12 15:11:34 -0400

    server not recognizing css files

--- npsweb/statics/views.py
+++ npsweb/statics/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponse
 import os
 
 # Create your views here.
 WK_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def statics(request, file):
-    return FileResponse(open(WK_DIR+'/static_files/'+file))
+def statics(request, file: str):
+    if file.endswith('.css'):
+        return HttpResponse(open(WK_DIR+'/static_files/'+file), content_type='text/css')
+    return FileResponse(open(WK_DIR+'/static_files/'+file, 'rb'))
