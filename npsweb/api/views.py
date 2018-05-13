06d96cbc7c05230577f660b0965feef392c02d80
cgood404 <chrisgood404@gmail.com>
2018-05-12 23:15:32 -0400

    last changes for today

--- npsweb/api/views.py
+++ npsweb/api/views.py
@@ -9,6 +9,37 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="/home/login")
 def es(request: HttpRequest):
+    """
+    GET params:
+        index: "comment", "reviews", "park", or "picture"
+        parkid: currently only the numbers 1-10, maps to a specific park
+        tmin: minimum unix epoch time to search by
+        tmax: maximum unix epoch time to search by
+
+    POST params:
+        index: "comment", "reviews", "park", or "picture"
+        parkid: currently only the numbers 1-10, maps to a specific park
+
+        if parkid is "comment":
+            pictureid: numeric id of the picture the comment is mapped to
+            comment: full comment as a string
+
+        else if parkid is "reviews":
+            parkid: id of park the review is for
+            review: full review as a string
+            rating: 1-5 integer
+
+        else if parkid is "park": # NOTE THAT USER MUST BE AN ADMIN TO POST A PARK
+            name: full name of the park
+            location: street address of the park
+
+        else if parkid is "picture":
+            parkid: id of park to map picture to
+            pictureurl: name of the picture file on the S3 bucket
+
+    :param request:
+    :return:
+    """
     if request.method == "GET" and\
             "index" in request.GET and\
             "parkid" in request.GET:
