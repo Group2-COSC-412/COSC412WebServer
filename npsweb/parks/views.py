d384730560439e1a562dc279330f6546c73f2f24
cgood404 <chrisgood404@gmail.com>
2018-05-21 19:24:54 -0400

    set up urls

--- npsweb/parks/views.py
+++ npsweb/parks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import FileResponse
+import os
 
 # Create your views here.
+WK_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# open(WK_DIR+'/html_files/'+file)
+def state(request, state: str, file: str):
+    if not file.endswith(".html"):
+        file += ".html"
+    return FileResponse(render(request, os.path.join(WK_DIR, state, file)))
