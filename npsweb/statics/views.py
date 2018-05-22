84c410f6b7902bc0dd6c5a39b9a2547d322447d9
cgood404 <chrisgood404@gmail.com>
2018-05-22 19:16:31 -0400

    trying to fix things

--- npsweb/statics/views.py
+++ npsweb/statics/views.py
@@ -11,4 +11,6 @@ def statics(request, file: str):
         return HttpResponse(open(WK_DIR+'/static_files/'+file), content_type='text/css')
     elif file.endswith('.js'):
         return HttpResponse(open(WK_DIR+'/static_files/'+file), content_type='application/javascript')
+    elif file.endswith('.zip'):
+        return HttpResponse(open(WK_DIR+'/static_files/'+file, 'rb').read, content_type='application/zip')
     return FileResponse(open(WK_DIR+'/static_files/'+file, 'rb'))
