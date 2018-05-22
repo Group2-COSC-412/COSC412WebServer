b100b32c2a5301de9d0dac97ffdb84c18cd4de0b
cgood404 <chrisgood404@gmail.com>
2018-05-22 14:54:26 -0400

    added reviews

--- npsweb/parks/views.py
+++ npsweb/parks/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import FileResponse
+from django.contrib.auth.decorators import login_required
+
 import os
 
 # Create your views here.
 WK_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-# open(WK_DIR+'/html_files/'+file)
+@login_required(login_url="/home/login")
 def state(request, _state: str, file: str):
     if not file.endswith(".html"):
         file += ".html"
     return FileResponse(render(request, os.path.join(_state, file)))
 
 
+@login_required(login_url="/home/login")
 def stateindex(request, _state: str):
     return FileResponse(render(request, os.path.join(_state, 'index.html')))
